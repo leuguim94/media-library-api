dfb48b571d45ffd6221e09f88de83bb6314e69d9
Miguel Valenzuela <leuguim94@gmail.com>
2020-04-24 20:26:53 +0200

    Added expiration date before create

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,14 @@
 class Purchase < ApplicationRecord
+  belongs_to :user
+  belongs_to :movie, required: false
+  belongs_to :season, required: false
+  
+  enum quality: %i(SD HD)
+
+  before_create :set_expire_date
+
+  # Set expire date before creating the purchase
+  def set_expire_date
+    self.expire_date =  DateTime.now + 3.days
+  end
 end
